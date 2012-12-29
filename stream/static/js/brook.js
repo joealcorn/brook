2babdd3645c3aa533f22d63f2aa6c24c570cb740
Joe Alcorn <me@buttscicl.es>
2012-12-29 15:16:38 +0000

    Set min-height on #right-panel to the height of the browser window or size of #left-panel, whichever is larger

--- stream/static/js/brook.js
+++ stream/static/js/brook.js
@@ -13,7 +13,20 @@ function update() {
     });
 };
 
+function set_min_height() {
+    var padding = parseInt($('.panel').css('padding-top')) + parseInt($('.panel').css('padding-bottom'))
+    
+    if (window.innerHeight - padding < $('#left-panel').outerHeight() - padding) {
+        $('#right-panel').css('min-height', $('#left-panel').outerHeight() - padding + 'px')
+    } else {
+        $('#right-panel').css('min-height', window.innerHeight - padding + 'px')
+    }
+};
+
+$(window).resize(set_min_height)
+
 $(document).ready(function() {
     $('.timestamp').timeago()
+    set_min_height()
     setInterval(update, interval)
 });
